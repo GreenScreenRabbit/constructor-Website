7b1cf7dab50a787c7188a678a1dd3ed7011a4b68
GreenScreenRabbit <77641729+GreenScreenRabbit@users.noreply.github.com>
2021-07-27 17:40:41 +0300

    Update asyncActions.tsx

--- src/actions and const/asyncActions.tsx
+++ src/actions and const/asyncActions.tsx
@@ -1,29 +1,6 @@
 import { connect, InferableComponentEnhancerWithProps } from 'react-redux';
 import { actions, ActionsTypes } from './actions'
-import { reduxForm, InjectedFormProps, Field } from 'redux-form';
-import { Dispatch } from 'redux'
-import { ThunkAction } from 'redux-thunk';
-import { mainSliderStateType } from '../rootReducer';
-import { Action } from 'redux'
-import { SET_POSITION_MAIN_SLIDER } from './const';
-import React, { Component }  from 'react';
 
-
-
-type TypeOfConnect<T> = T extends InferableComponentEnhancerWithProps<infer Props, infer _>
-  ? Props
-  : never
-  ;
-
-
-type Promese = {
-  post: any
-}
-
-
-function timeout(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 
 export const asyncActions = {
